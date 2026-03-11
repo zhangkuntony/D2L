059d0eb1147b9c4ae2b70b69cc460f2a3fe6b913
zhangkun <kun.zhang@wocheng.tv>
2026-03-11 23:19:21 +0800

    Add chapter 4 code

--- d2l/d2l_torch.py
+++ d2l/d2l_torch.py
@@ -63,6 +63,41 @@ def sgd(params, lr, batch_size):
 
 
 # ==================== 可视化 ====================
+def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
+         ylim=None, xscale='linear', yscale='linear',
+         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(3.5, 2.5), axes=None):
+    """绘制数据点"""
+    if legend is None:
+        legend = []
+
+    set_figsize(figsize)
+    axes = axes if axes else plt.gca()
+
+    # 如果X有一个轴，输出True
+    def has_one_axis(X):
+        return (hasattr(X, 'ndim') and X.ndim == 1 or isinstance(X, list)
+                and not hasattr(X[0], '__len__'))
+
+    if has_one_axis(X):
+        X = [X]
+
+    if Y is None:
+        X, Y = [[]] * len(X), X
+    elif has_one_axis(Y):
+        Y = [Y]
+
+    if len(X) != len(Y):
+        X = X * len(Y)
+
+    axes.cla()
+    for x, y, fmt in zip(X, Y, fmts):
+        if len(x):
+            axes.plot(x, y, fmt)
+        else:
+            axes.plot(y, fmt)
+
+    set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
 from IPython import display
 
 class Animator:
